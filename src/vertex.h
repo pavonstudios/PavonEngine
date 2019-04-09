bdff39fb913aed97f44ea1c0297d94331da74c4e
Oscar Pavon <pavon_oscar@outlook.com>
2019-04-09 05:56:03 -0400

    fixed attribute description location

--- src/vertex.h
+++ src/vertex.h
@@ -53,12 +53,12 @@ struct Vertex {
         attributeDescriptions[2].offset = offsetof(Vertex, texCoord);
 
         attributeDescriptions[3].binding = 0;
-        attributeDescriptions[3].location = 2;
+        attributeDescriptions[3].location = 3;
         attributeDescriptions[3].format = VK_FORMAT_R32G32B32_SFLOAT;
         attributeDescriptions[3].offset = offsetof(Vertex, joint0);
 
         attributeDescriptions[4].binding = 0;
-        attributeDescriptions[4].location = 2;
+        attributeDescriptions[4].location = 4;
         attributeDescriptions[4].format = VK_FORMAT_R32G32B32_SFLOAT;
         attributeDescriptions[4].offset = offsetof(Vertex, weight0);
 
