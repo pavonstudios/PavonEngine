ccbf23af3b17494e77ccfc0ac7cf2b58afe0b6ef
Oscar Pavon <pavon_oscar@outlook.com>
2019-04-16 14:40:00 -0400

    triangle

--- src/android_renderer.h
+++ src/android_renderer.h
@@ -112,6 +112,7 @@ private:
     GLuint indices;
 
     GLuint textureid;
+    GLuint texture2id;
 
     GLint
             phase_loc,
@@ -234,9 +235,59 @@ public:
 
         load_shaders();
 
-         init_3d_model();
-  
+        // init_3d_model();
+        create_triangule();
+        create_texture();
 
+    }
+    void create_triangule(){
+        
+
+        EMesh* triangle = new EMesh();
+        glm::vec2 texcoor = glm::vec2(0.f);
+        glm::vec4 posss = glm::vec4(0.f);
+        glm::vec4 witjj = glm::vec4(0.f);
+        Vertex vert1{};
+        vert1.pos = glm::vec3(1.0,1.0,0.0);
+        Vertex vert2{};
+        vert2.pos = glm::vec3(0.0,0.0,0.0);
+        Vertex vert3{};
+        vert3.pos = glm::vec3(0.0,1.0,0.0);
+
+        vert1.texCoord = glm::vec2(0.0f,0.0f);
+        vert1.joint0 = glm::vec4(0.0f);
+        vert1.weight0 = glm::vec4(0.0f);
+
+           vert3.texCoord = glm::vec2(0.0f,1.0f);
+        vert3.joint0 = glm::vec4(0.0f);
+        vert3.weight0 = glm::vec4(0.0f);
+
+           vert2.texCoord = glm::vec2(1.0f,1.0f);
+        vert2.joint0 = glm::vec4(0.0f);
+        vert2.weight0 = glm::vec4(0.0f);
+
+        triangle->vertices.push_back(vert1);
+        triangle->vertices.push_back(vert2);
+        triangle->vertices.push_back(vert3);
+
+                float vertices[] = {
+            // positions          // colors           // texture coords
+            -0.5f,  0.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+            0.5f, 0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f   // bottom left
+           // -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+        };
+
+        glGenBuffers(1,&vertex_buffer);
+        glBindBuffer(GL_ARRAY_BUFFER,vertex_buffer);
+        glBufferData(GL_ARRAY_BUFFER,8 * sizeof(float),vertices,GL_STATIC_DRAW);
+
+        glVertexAttribPointer ( 0, 3, GL_FLOAT, false, sizeof(float), (void*)0 );
+        glEnableVertexAttribArray ( 0 ); 
+        glVertexAttribPointer ( 1, 3, GL_FLOAT, false, sizeof(float), (void*)(3*sizeof(float)) );
+        glEnableVertexAttribArray ( 1 );
+       // glVertexAttribPointer ( 2, 2, GL_FLOAT, false, 8 * sizeof(float), (void*)(6*sizeof(float)) );
+        //glEnableVertexAttribArray ( 2 );
     }
   
     void init_3d_model(){
@@ -249,7 +300,6 @@ public:
         #endif
     
         meshes.push_back(mesh);
-
 
         glGenBuffers(1,&vertex_buffer);
         glBindBuffer(GL_ARRAY_BUFFER,vertex_buffer);
@@ -281,7 +331,7 @@ public:
     }
     void create_texture(){
              
-
+        glEnable( GL_TEXTURE_2D ); 
         #ifdef ANDROID
             AssetManager assets;
             textureid = assets.load_bmp("patrol.bmp",app->activity->assetManager);
@@ -290,27 +340,27 @@ public:
             
             
             AssetManager assets;
-            image_size size = assets.load_and_get_size("textures/car01.jpg");
+            image_size size = assets.load_and_get_size("/home/pavon/Textures/coust/modules_coust.png");
             glActiveTexture(GL_TEXTURE0);
             glGenTextures(1, &textureid);
             glBindTexture(GL_TEXTURE_2D,textureid);
             
                      
-            //glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,size.width,size.heigth,0,GL_RGB,GL_UNSIGNED_BYTE,size.pPixels);
+            glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,size.width,size.heigth,0,GL_RGB,GL_UNSIGNED_BYTE,size.pPixels);
 
             float pixels[] = {
-            0.0f, 0.0f, 0.0f,   1.0f, 1.0f, 1.0f,
-            1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f
+            0.0f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f
             };
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
+           // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
           
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+           // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);       
             glGenerateMipmap(GL_TEXTURE_2D);
              
-            glEnable( GL_TEXTURE_2D ); 
+            
         
         #endif
     }
@@ -333,7 +383,7 @@ public:
             glUseProgram  ( shaderProgram );
 
              
-            update_mvp();
+            //update_mvp();
             draw_mesh();
             
 
@@ -354,8 +404,8 @@ public:
                 glm::mat4 Projection = glm::perspective(glm::radians(45.f), 768.f/1280.f, 0.01f, 1000.f);
                 glm::mat4 view = glm::lookAt(vec3(0,15,0),vec3(0,0,0),vec3(0,0,1));
 
-                model = glm::rotate(model, time * glm::radians(12.f), glm::vec3(0.0f, 1.0f, 1.0f));
-
+               // model = glm::rotate(model, time * glm::radians(12.f), glm::vec3(0.0f, 1.0f, 1.0f));
+                model = rotate(model, radians(90.f),vec3(1.0f,0.0f,0.0f));
                 mat4 mvp = Projection * view * model;
 
                 glUniformMatrix4fv(0,1,GL_FALSE,&mvp[0][0]);
@@ -371,7 +421,10 @@ public:
             glUniform1i(samplerid, 0);            
             
             
-            glDrawElements(GL_TRIANGLES,meshes[0]->indices.size(),GL_UNSIGNED_INT,(void*)0);
+            //glDrawElements(GL_TRIANGLES,meshes[0]->indices.size(),GL_UNSIGNED_INT,(void*)0);
+
+            glDrawArrays(GL_TRIANGLES,0,3);
+            //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     }
     
 };
