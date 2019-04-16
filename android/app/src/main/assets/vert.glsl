e7110d756c1735b556eb71e146b388d66cc92690
Oscar Pavon <pavon_oscar@outlook.com>
2019-04-15 20:35:50 -0400

    fixed error android shader with tab

--- android/app/src/main/assets/vert.glsl
+++ android/app/src/main/assets/vert.glsl
@@ -11,6 +11,6 @@ uniform mat4          MVP;
 void main()                         
 {                                   
 	gl_Position = MVP * vec4(position,1.0);   
-	v_TexCoord = vec2(inUV.x, inUV.y); 
-	color = inColor;             
-}                        
+	v_TexCoord = vec2(inUV.x, inUV.y);
+	color = inColor;
+}
