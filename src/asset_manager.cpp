0e3782526a8a9ec9c20baf592431e6f445886611
Oscar Pavon <pavon_oscar@outlook.com>
2019-04-16 22:54:44 -0400

    fixed vulkan render error in image loader

--- src/asset_manager.cpp
+++ src/asset_manager.cpp
@@ -8,9 +8,10 @@ void AssetManager::free_image(void * pixels){
 }
 image_size AssetManager::load_and_get_size(std::string texture_path){
     int texWidth, texHeight, texChannels;
-    stbi_uc* pixels = stbi_load(texture_path.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb);
+    stbi_uc* pixels = stbi_load(texture_path.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
         
     unsigned char* data = stbi_load(texture_path.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb);
+   
     if (!pixels) {
             std::string error = "failed to load texture image! " + texture_path;
             throw std::runtime_error(error);
