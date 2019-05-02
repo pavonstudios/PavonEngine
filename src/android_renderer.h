9a784907c8709d21adf695b2ccfa6e7fa908dd19
Oscar Pavon <pavon_oscar@outlook.com>
2019-05-02 13:57:59 -0400

    multi objetcs

--- src/android_renderer.h
+++ src/android_renderer.h
@@ -391,35 +391,34 @@ void activate_vertex_attributes(EMesh* mesh){
         
         for(EMesh* mesh: meshes){ 
             if(mesh->texture.texture_id == -1){
-
           
-            glGenTextures(1, &mesh->texture_id);
-            glBindTexture(GL_TEXTURE_2D,mesh->texture_id);
+                glGenTextures(1, &mesh->texture_id);
+                glBindTexture(GL_TEXTURE_2D,mesh->texture_id);
 
 
-            AssetManager assets;
-            #ifdef ANDROID
-                
-                image_size size = assets.load_bmp("police_patrol.pvn",app->activity->assetManager);    //TODO: load texture with android path        
-            #else                   
-                image_size size;
-                if(mesh->texture.hasTexture){
-                    size.heigth = mesh->texture.height;
-                    size.width = mesh->texture.width;
-                    size.data = mesh->texture.data;
-                    glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,size.width,size.heigth,0,GL_RGBA,GL_UNSIGNED_BYTE,size.data);
-                }else{
-                    size = assets.load_and_get_size(mesh->texture_path.c_str());
-                    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,size.width,size.heigth,0,GL_RGB,GL_UNSIGNED_BYTE,size.data); 
-                }
+                AssetManager assets;
+                #ifdef ANDROID
                     
-            #endif                   
-            glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-            glGenerateMipmap(GL_TEXTURE_2D);
+                    image_size size = assets.load_bmp("police_patrol.pvn",app->activity->assetManager);    //TODO: load texture with android path        
+                #else                   
+                    image_size size;
+                    if(mesh->texture.hasTexture){
+                        size.heigth = mesh->texture.height;
+                        size.width = mesh->texture.width;
+                        size.data = mesh->texture.data;
+                        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,size.width,size.heigth,0,GL_RGBA,GL_UNSIGNED_BYTE,size.data);
+                    }else{
+                        size = assets.load_and_get_size(mesh->texture_path.c_str());
+                        glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,size.width,size.heigth,0,GL_RGB,GL_UNSIGNED_BYTE,size.data); 
+                    }
+                        
+                #endif                   
+                glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+                glGenerateMipmap(GL_TEXTURE_2D);
 
             }else{
                 mesh->texture_id = textures_ids[mesh->texture.texture_id];
