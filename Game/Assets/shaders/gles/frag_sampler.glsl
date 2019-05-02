b427172d17d011cf302ba54c79ba74cc1e4b189e
Oscar Pavon <pavon_oscar@outlook.com>
2019-05-02 17:40:33 -0400

    android texture

---
+++ Game/Assets/shaders/gles/frag_sampler.glsl
@@ -0,0 +1,6 @@
+#version 100
+precision highp float;
+uniform sampler2D texture_sampler;
+varying vec3 color;
+varying vec2 v_TexCoord;
+void main(){gl_FragColor = texture2D(texture_sampler,v_TexCoord);}
--- Game/Assets/vert_mvp.glsl
+++ Game/Assets/vert_mvp.glsl
@@ -1,11 +1,11 @@
 #version 100
 attribute mediump vec3        	position;
-attribute mediump vec3			inColor;          
+attribute mediump vec3			inColor;
 attribute mediump vec2          inUV;
 varying mediump vec2 v_TexCoord;
-varying mediump vec3 color;       
+varying mediump vec3 color;
 uniform mat4          MVP;
-void main()
-{    gl_Position = MVP * vec4(position,1.0);
-    v_TexCoord = inUV;
-    color = inColor;}
+void main(){
+gl_Position = MVP * vec4(position,1.0);
+v_TexCoord = inUV;
+color = inColor;}
