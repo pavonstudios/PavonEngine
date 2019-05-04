014251c2c3c7b5caaa57ca48774242ac95e3357f
Oscar Pavon <pavon_oscar@outlook.com>
2019-05-04 10:06:57 -0400

    end line of shader alway return error

--- src/android_renderer.h
+++ src/android_renderer.h
@@ -38,6 +38,15 @@
 using  namespace engine;
 using namespace std;
 using namespace glm;
+
+struct shader_src{
+    std::string vertex_shader_path;
+    std::string fragment_shader_path;
+    const char* vertex_code;
+    const char* fragment_code;
+    int type = -1;
+};
+
 class Renderer{
 public:
     bool bReady = false;
@@ -45,6 +54,10 @@ public:
     EMesh* triangle;
      GLuint vertex_buffer;
      std::vector<GLuint> textures_ids;
+#ifdef ANDROID
+    struct android_app * app;
+#endif
+
 private:
 
 
@@ -56,21 +69,30 @@ private:
 
     GLuint shaderProgram;
 
-    GLuint load_shader (const char  *shader_source, GLenum type)
+    GLuint load_shader (shader_src &shader_data, GLenum type)
     {
-        //LOGW("Shader source:\n %s",shader_source);
+        const char* shader_source;
+        if(type == GL_VERTEX_SHADER){
+            shader_source = shader_data.vertex_code;
+            shader_data.type = 0;
+        }
+        if( type == GL_FRAGMENT_SHADER){
+            shader_source = shader_data.fragment_code;
+            shader_data.type = 1;
+        }
+       
         GLuint  shader = glCreateShader( type );
 
         glShaderSource  ( shader , 1 , &shader_source , NULL );
         glCompileShader ( shader );
 
-       // LOGW("Shader safter compile :\n %s",shader_source);
-        print_shader_info_log ( shader , shader_source);
+       
+        print_shader_info_log ( shader , shader_data , shader_source);
 
         return shader;
     }
 
-    void print_shader_info_log (GLuint shader, const char* src){
+    void print_shader_info_log (GLuint shader, shader_src &shader_data, const char* src){
 
         GLint  length;
 
@@ -80,19 +102,35 @@ private:
             char* buffer  =  new char [ length ];
             glGetShaderInfoLog ( shader , length , NULL , buffer );
             #ifdef ANDROID
-                LOGW("shader info %s ",buffer);
+                LOGW("------------->SHADER info %s ",buffer);
             #else
-                cout << "shader info: " <<  buffer << flush;
+                cout << "SHADER info: " <<  buffer << flush;
             #endif
             delete [] buffer;
 
             GLint success;
             glGetShaderiv( shader, GL_COMPILE_STATUS, &success );
             if ( success != GL_TRUE ) {
+
+               
                 #ifdef ANDROID
                     LOGW("ERROR in shader loader");
+                    if(shader_data.type == 0){
+                        std::cout << "Error in file " <<shader_data.vertex_shader_path << std::endl;
+                        LOGW("ERROR in file %s",shader_data.vertex_shader_path.c_str());
+                    }
+                    if(shader_data.type == 1){
+                        std::cout << "Error in file " << shader_data.fragment_shader_path << std::endl;
+                        LOGW("ERROR in file %s",shader_data.fragment_shader_path.c_str());
+                    }
                 #endif
                 #ifdef ES2
+                    if(shader_data.type == 0){
+                        std::cout << "Error in file " <<shader_data.vertex_shader_path << std::endl;
+                    }
+                    if(shader_data.type == 1){
+                        std::cout << "Error in file " << shader_data.fragment_shader_path << std::endl;
+                    }
                     exit ( 1 );
                 #endif
             }
@@ -104,7 +142,7 @@ private:
             AAsset* file = AAssetManager_open(app->activity->assetManager,path, AASSET_MODE_BUFFER);
 
             size_t file_length = AAsset_getLength(file);
-            char* fileContent = new char[file_length-1];
+            char* fileContent = new char[file_length+1];
 
             AAsset_read(file, fileContent,file_length);
             AAsset_close(file);
@@ -134,31 +172,7 @@ private:
             return char_content;
         #endif
     }
-    
-    void load_shaders(){
 
-        #ifdef ANDROID
-            char* vertex_shader_src = load_shader_file("vert.glsl");
-            char* fragment_shader_src = load_shader_file("frag_vert_color.glsl");
-            //LOGW("%s",vertex_shader_src);
-        #else
-            char* vertex_shader_src = load_shader_file("android/app/src/main/assets/vert.glsl");
-            char* fragment_shader_src = load_shader_file("android/app/src/main/assets/frag.glsl");
-        #endif
-
-        GLuint vertexShader   = load_shader ( vertex_shader_src , GL_VERTEX_SHADER  );     // load vertex shader
-
-
-        GLuint fragmentShader = load_shader ( fragment_shader_src , GL_FRAGMENT_SHADER );  // load fragment shader
-
-        
-        shaderProgram = glCreateProgram ();                 // create program object
-        glAttachShader ( shaderProgram, vertexShader );             // and attach both...
-        glAttachShader ( shaderProgram, fragmentShader );           // ... shaders to it
-
-        glLinkProgram ( shaderProgram );    // link the program
-       
-    }
      
 public:
 
@@ -167,9 +181,15 @@ public:
         char* vertex_shader_src = load_shader_file(mesh->data_shader.vertex_shader_path.c_str());
         char* fragment_shader_src = load_shader_file(mesh->data_shader.fragment_shader_path.c_str());
          
+      shader_src shader;
+      shader.vertex_shader_path = mesh->data_shader.vertex_shader_path;
+      shader.fragment_shader_path = mesh->data_shader.fragment_shader_path;
+        shader.vertex_code = vertex_shader_src;
+        shader.fragment_code = fragment_shader_src;
 
-        GLuint vertexShader   = load_shader ( vertex_shader_src , GL_VERTEX_SHADER  );     // load vertex shader
-        GLuint fragmentShader = load_shader ( fragment_shader_src , GL_FRAGMENT_SHADER );  // load fragment shader
+
+        GLuint vertexShader   = load_shader ( shader , GL_VERTEX_SHADER  );     // load vertex shader
+        GLuint fragmentShader = load_shader ( shader , GL_FRAGMENT_SHADER );  // load fragment shader
 
         
         mesh->shader_program = glCreateProgram ();                 // create program object
@@ -185,9 +205,15 @@ public:
              char* vertex_shader_src = load_shader_file(mesh->data_shader.vertex_shader_path.c_str());
             char* fragment_shader_src = load_shader_file(mesh->data_shader.fragment_shader_path.c_str());
             
+        shader_src shader;
+            shader.vertex_shader_path = mesh->data_shader.vertex_shader_path;
+            shader.fragment_shader_path = mesh->data_shader.fragment_shader_path;
+                shader.vertex_code = vertex_shader_src;
+                shader.fragment_code = fragment_shader_src;
 
-            GLuint vertexShader   = load_shader ( vertex_shader_src , GL_VERTEX_SHADER  );     // load vertex shader
-            GLuint fragmentShader = load_shader ( fragment_shader_src , GL_FRAGMENT_SHADER );  // load fragment shader
+
+        GLuint vertexShader   = load_shader ( shader , GL_VERTEX_SHADER  );     // load vertex shader
+        GLuint fragmentShader = load_shader ( shader , GL_FRAGMENT_SHADER );  // load fragment shader
 
             
             mesh->shader_program = glCreateProgram ();                 // create program object
@@ -206,150 +232,15 @@ public:
         
        
         glEnable(GL_DEPTH_TEST);
-        glDepthFunc(GL_LESS);           
-
-        
-        #ifdef ANDROID
-            //load_shaders();
-            //init_3d_model();
-            //create_triangule();
-            //create_texture();
-        #endif
-        //create_triangule();
+        glDepthFunc(GL_LESS);
 
     }
-    void create_triangule(){
-        
 
-        EMesh* triangle = new EMesh();
-        glm::vec2 texcoor = glm::vec2(0.f);
-        glm::vec4 posss = glm::vec4(0.f);
-        glm::vec4 witjj = glm::vec4(0.f);
-        Vertex vert1{};
-        vert1.pos = glm::vec3(1.0,1.0,0.0);
-        Vertex vert2{};
-        vert2.pos = glm::vec3(0.0,0.0,0.0);
-        Vertex vert3{};
-        vert3.pos = glm::vec3(0.0,1.0,0.0);
-
-        vert1.texCoord = glm::vec2(1.0f,1.0f);
-        vert1.color = vec3(1.0,0.0,0.0);
-        vert1.joint0 = glm::vec4(0.0f);
-        vert1.weight0 = glm::vec4(0.0f);
-
-           vert3.texCoord = glm::vec2(0.0f,0.0f);
-        vert3.joint0 = glm::vec4(0.0f);
-         vert3.color = vec3(0.0,0.0,1.0);
-        vert3.weight0 = glm::vec4(0.0f);
-
-           vert2.texCoord = glm::vec2(1.0f,0.0f);
-        vert2.joint0 = glm::vec4(0.0f);
-         vert2.color = vec3(0.0,1.0,0.0);
-        vert2.weight0 = glm::vec4(0.0f);
-
-        triangle->vertices.push_back(vert1);
-        triangle->vertices.push_back(vert2);
-        triangle->vertices.push_back(vert3);
-
-                float vertices[] = {
-            // positions          // colors           // texture coords
-            -0.5f,  0.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-            0.5f, 0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f   // bottom left
-           // -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
-        };
-
-        glGenBuffers(1,&vertex_buffer);
-        glBindBuffer(GL_ARRAY_BUFFER,vertex_buffer);
-        glBufferData(GL_ARRAY_BUFFER,triangle->vertices.size() * sizeof(Vertex),triangle->vertices.data(),GL_STATIC_DRAW);
-
-        glVertexAttribPointer ( 0, 3, GL_FLOAT, false, sizeof(Vertex), (void*)0 );
-        glEnableVertexAttribArray ( 0 ); 
-        glVertexAttribPointer ( 1, 3, GL_FLOAT, false, sizeof(Vertex), (void*)offsetof(Vertex,color) );
-        glEnableVertexAttribArray ( 1 );
-        glVertexAttribPointer ( 2, 2, GL_FLOAT, false, sizeof(Vertex), (void*)offsetof(Vertex,texCoord) );
-        glEnableVertexAttribArray ( 2 );
-
-        this->triangle = triangle;
-    }
   
-    void init_3d_model(){
-        EMesh* mesh = new EMesh();
-        int mesh_load_result;
-        #ifdef ANDROID
-           // mesh_load_result = mesh->load_mode_gltf_android("police_patrol.gltf",app->activity->assetManager);
-        #else//ES2
-            //mesh_load_result = engine->mesh_manager.load_model_gltf(mesh, "android/app/src/main/assets/police_patrol.gltf");
-        #endif
+
+
+
     
-       
-        glGenBuffers(1,&vertex_buffer);
-        glBindBuffer(GL_ARRAY_BUFFER,vertex_buffer);
-        glBufferData(GL_ARRAY_BUFFER,mesh->vertices.size() * sizeof(Vertex),mesh->vertices.data(),GL_STATIC_DRAW);
-
-        glGenBuffers(1,&indices);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,indices);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER,mesh->indices.size() * sizeof(unsigned int),mesh->indices.data(), GL_STATIC_DRAW);
-
-        create_texture();       
-       
-        int vPos = glGetAttribLocation(shaderProgram,"position");
-        int incolor = glGetAttribLocation(shaderProgram,"inColor");        
-        int incood = glGetAttribLocation(shaderProgram,"inUV");
-        int samplerid = glGetUniformLocation(shaderProgram, "texture_sampler");
-         
-        activate_vertex_attributes(mesh);
-    }
-
-void activate_vertex_attributes(EMesh* mesh){
-
-        glBindBuffer(GL_ARRAY_BUFFER,mesh->vertex_buffer);
-
-        glVertexAttribPointer ( 0, 3, GL_FLOAT, false, sizeof(Vertex), (void*)0 );
-        glEnableVertexAttribArray ( 0 ); 
-
-        glVertexAttribPointer ( 1, 3, GL_FLOAT, false, sizeof(Vertex), (void*)offsetof(Vertex,color) );
-        glEnableVertexAttribArray ( 1 );            
-        
-        glVertexAttribPointer ( 2, 2, GL_FLOAT, false, sizeof(Vertex), (void*)offsetof(Vertex,texCoord) );
-        glEnableVertexAttribArray ( 2 );
-
-    }
-    void create_texture(){
-             
-
-        #ifdef ANDROID
-            AssetManager assets;
-            image_size size = assets.load_bmp("police_patrol_image.pvn",app->activity->assetManager);            
-        #else                   
-            AssetManager assets;
-            image_size size = assets.load_and_get_size("textures/skydome.jpg");      
-        #endif
-            glActiveTexture(GL_TEXTURE0);
-            glGenTextures(1, &textureid);
-            glBindTexture(GL_TEXTURE_2D,textureid);
-                     
-            //glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,size.width,size.heigth,0,GL_RGB,GL_UNSIGNED_BYTE,size.data);
-
-            float pixels[] = {
-            255.0f, 0.0f, 0.0f,1.0f ,   0.0f, 255.0f, 0.0f, 1.0f,
-            0.0f, 255.0f, 0.0f,1.0f ,   0.0f, 0.0f, 255.0f,1.0f
-
-            };
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 2, 2, 0, GL_RGBA, GL_FLOAT, pixels);
-            glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-            //glGenerateMipmap(GL_TEXTURE_2D);
-
-    }
-    
-    #ifdef ANDROID
-    struct android_app * app;
-    #endif
 
    public:
    void load_mesh_texture(EMesh* mesh){
@@ -457,6 +348,22 @@ void activate_vertex_attributes(EMesh* mesh){
            
     }
 
+
+    void activate_vertex_attributes(EMesh* mesh){
+
+        glBindBuffer(GL_ARRAY_BUFFER,mesh->vertex_buffer);
+
+        glVertexAttribPointer ( 0, 3, GL_FLOAT, false, sizeof(Vertex), (void*)0 );
+        glEnableVertexAttribArray ( 0 );
+
+        glVertexAttribPointer ( 1, 3, GL_FLOAT, false, sizeof(Vertex), (void*)offsetof(Vertex,color) );
+        glEnableVertexAttribArray ( 1 );
+
+        glVertexAttribPointer ( 2, 2, GL_FLOAT, false, sizeof(Vertex), (void*)offsetof(Vertex,texCoord) );
+        glEnableVertexAttribArray ( 2 );
+
+    }
+
        void render(){                    
 
             #ifdef ANDROID
@@ -467,26 +374,6 @@ void activate_vertex_attributes(EMesh* mesh){
 
     };
 
-    void update_mvp(){
-       
-        {  //mvp 
-                static auto startTime = std::chrono::high_resolution_clock::now();
-                auto currentTime = std::chrono::high_resolution_clock::now();
-                float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
-
-                glm::mat4 model = glm::mat4(1.0);
-                glm::mat4 Projection = glm::perspective(glm::radians(45.f), 768.f/1280.f, 0.01f, 5000.f);
-                glm::mat4 view = glm::lookAt(vec3(0,15,0),vec3(0,0,0),vec3(0,0,1));
-
-                model = glm::rotate(model, time * glm::radians(12.f), glm::vec3(0.0f, 1.0f, 1.0f));
-                //model = rotate(model, radians(90.f),vec3(1.0f,0.0f,0.0f));
-                mat4 mvp = Projection * view * model;
-
-                glUniformMatrix4fv(0,1,GL_FALSE,&mvp[0][0]);
-            }
-             
-  
-    }
 
     void update_mvp(EMesh* mesh){
                                 
